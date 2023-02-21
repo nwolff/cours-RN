919a12e62eee4a31d04bdfda4d04102d6b98ff6e
Nicholas Wolff <nwolff@gmail.com>
2023-02-21 18:34:31 +0100

    requirements for macs

--- script.py
+++ script.py
@@ -6,12 +6,11 @@ import matplotlib.pyplot as plt
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-plt.figure(figsize=(10,10)),
+plt.figure(figsize=(10, 10)),
 for i in range(16):
-    plt.subplot(4,4,i+1)
-    plt.imshow(x_train[i], cmap='binary')
+    plt.subplot(4, 4, i + 1)
+    plt.imshow(x_train[i], cmap="binary")
     plt.xlabel(str(y_train[i]))
     plt.xticks([])
     plt.yticks([])
 plt.show()
-
