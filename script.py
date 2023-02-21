283cf9c371f6d6cdfcf645e74ec5951f7a5b8cac
Nicholas Wolff <nwolff@gmail.com>
2023-02-21 18:07:25 +0100

    first commit

---
+++ script.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+plt.figure(figsize=(10,10)),
+for i in range(16):
+    plt.subplot(4,4,i+1)
+    plt.imshow(x_train[i], cmap='binary')
+    plt.xlabel(str(y_train[i]))
+    plt.xticks([])
+    plt.yticks([])
+plt.show()
+
