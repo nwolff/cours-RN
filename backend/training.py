e5f5bb421063375ce6e681d2397b329beb7aca53
Nicholas Wolff <nwolff@gmail.com>
2023-03-12 14:22:26 +0100

    Repeatable results

--- backend/training.py
+++ backend/training.py
@@ -1,56 +1,42 @@
 #!/usr/bin/env python
-import matplotlib.pyplot as plt
-import numpy as np
-from image import IMAGE_SIZE, resize
 from tensorflow import keras
 
-
-def show_training_data_sample(images, labels):
-    plt.figure(figsize=(10, 10)),
-    for i in range(16):
-        plt.subplot(4, 4, i + 1)
-        plt.imshow(images[i], cmap="binary")
-        plt.xlabel(str(labels[i]))
-        plt.xticks([])
-        plt.yticks([])
-    plt.show()
-
+# For repeatable results
+keras.utils.set_random_seed(1)
+import numpy as np
+from image import IMAGE_SIZE, resize
 
 mnist = keras.datasets.mnist
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-print("resizing images...")
+print("Resizing images...")
 train_images = [resize(img) for img in train_images]
 test_images = [resize(img) for img in test_images]
 print("done")
-# show_training_data_sample(train_images, train_labels)
 
-# Unroll the pixel grids
-train_x = np.reshape(train_images, (60000, IMAGE_SIZE * IMAGE_SIZE))
-test_x = np.reshape(test_images, (10000, IMAGE_SIZE * IMAGE_SIZE))
+# Unroll the pixel grids and normalize values between 0 and 1
+train_x = np.reshape(train_images, (60000, IMAGE_SIZE * IMAGE_SIZE)) / 255
+test_x = np.reshape(test_images, (10000, IMAGE_SIZE * IMAGE_SIZE)) / 255
 
-# Normalize values from 0 to 1
-train_x = train_x / 255
-test_x = test_x / 255
-
+# ReLU instead of sigmoid for better results (but harder to draw neurons)
 model = keras.models.Sequential(
     [
-        keras.layers.Dense(
-            32,
-            activation="sigmoid",
-            use_bias=False,
-            input_shape=(IMAGE_SIZE * IMAGE_SIZE,),
-        ),
+        keras.layers.Input(shape=(IMAGE_SIZE * IMAGE_SIZE,)),
+        keras.layers.Dense(32, use_bias=False, activation="sigmoid"),
         keras.layers.Dense(32, use_bias=False, activation="sigmoid"),
         keras.layers.Dense(10, use_bias=False, activation="softmax"),
     ]
 )
 
+print(model.summary())
+
 model.compile(
-    loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"]
+    loss="sparse_categorical_crossentropy",  # Because our classes are mutually exclusive
+    optimizer="adam",
+    metrics=["accuracy"],
 )
 
-_ = model.fit(
+history = model.fit(
     train_x,
     train_labels,
     validation_data=(test_x, test_labels),
